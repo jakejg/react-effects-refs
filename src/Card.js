f9158f9e1fe949aa107959050dad80df60277980
Jake <jakefrisbee@gmail.com>
2020-05-24 12:17:38 -0600

    set up first API call

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = ({imgSrc}) => {
+
+    return (
+        <div>
+            <img src={imgSrc} />
+        </div>
+    )
+}
+
+export default Card;
--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Card from './Card'
 import axios from 'axios';
 
 
 const CardContainer = () => {
-    
+    const [cardImg, setCardImg] = useState(null)
+    const [url , setUrl] = useState("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
+
+    useEffect(() => {
+        const getCard = async () => {
+            const res = await axios.get(url);
+            setCardImg(data => res.data.cards[0].image);
+        }
+        getCard()
+    }, [url])
+
+
+    return (
+        <>
+            <Card imgSrc={cardImg} />
+            <button>Draw Card</button>
+        </>
+    )
 }
 
 export default CardContainer
