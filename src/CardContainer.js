7b2d1c3633a221d87a881dadf24d054627d21d79
Jake <jakefrisbee@gmail.com>
2020-05-25 09:14:44 -0600

    added start/stop card draw button

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -2,33 +2,56 @@ import React, { useState, useEffect } from "react";
 import Card from './Card'
 import axios from 'axios';
 
+const BASE_URL = "https://deckofcardsapi.com/api/deck"
+
 
 const CardContainer = () => {
-    const [cardData, setCardData] = useState({})
-    const [apiData , setApiData] = useState({url: "https://deckofcardsapi.com/api/deck/new/draw/?count=1", drawCount: 0})
+    const [cardData, setCardData] = useState({});
+    const [apiData , setApiData] = useState({url: `${BASE_URL}/new/draw/?count=1`, drawCount: 0});
+    const [draw, setDraw] = useState(false);
 
     useEffect(() => {
         const getCard = async () => {
-            const res = await axios.get(apiData.url);
-            if (res.data.remaining === 0) alert("No cards left");
-            else setCardData(cardData => ({imgSrc: res.data.cards[0].image, deckId: res.data.deck_id}));
+            try{
+                const res = await axios.get(apiData.url);
+                setCardData(cardData => ({imgSrc: res.data.cards[0].image, deckId: res.data.deck_id}));
+            }
+            catch(err){
+                alert(err)
+            }
         }
-        getCard()
+
+        if (apiData.drawCount > 51) {
+            setDraw(false);
+            alert("No cards left");
+        }
+        else getCard();
     }, [apiData])
 
 
-    const draw = () => {
-        setApiData(apiData => ({
-                    url: `https://deckofcardsapi.com/api/deck/${cardData.deckId}/draw/?count=1`,
-                    drawCount: apiData.draw + 1}
-        ))
+    useEffect(() => {
+        if (draw) {
+            const timerId = setInterval(() => {
+                setApiData(apiData => ({
+                    url: `${BASE_URL}/${cardData.deckId}/draw/?count=1`,
+                    drawCount: apiData.drawCount + 1}
+                ))
+            }, 1000)
+
+            return () => clearInterval(timerId)
+        }
+    }, [draw])
+        
+
+    const startStopDraw = () => {
+        setDraw(!draw)
     }
 
 
     return (
         <>
             <Card imgSrc={cardData.imgSrc} />
-            <button onClick={draw}>Draw Card</button>
+            <button onClick={startStopDraw}>{draw ? "Stop Draw": "Start Draw"}</button>
         </>
     )
 }
